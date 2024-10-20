a8e45fd89e5e1e8fbc53405448290491fabd58c8
hinusif <hinusif@proton.me>
2024-10-20 05:14:02 +0000

    Add IBM Plex Sans family, Cascadia Code PL and apply them as default fonts

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
   ignoreFiles: [
+    'app/javascript/styles/fonts/ibm-plex-sans-family.scss'
+    'app/javascript/styles/fonts/cascadia-code-pl.scss'
     'app/javascript/styles/mastodon/reset.scss',
     'coverage/**/*',
     'node_modules/**/*',
